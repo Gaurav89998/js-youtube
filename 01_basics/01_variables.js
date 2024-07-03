7b075d16f4da9299d8400819459bc747a54c05ad
Gaurav89998 <150925403+Gaurav89998@users.noreply.github.com>
2024-07-03 02:29:59 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "hc@hc.com"
+ accountPassword = "098765"
+ accountCity = "bengaluru"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState ])
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,25 @@
+"use strict";//treat all js code as newer version
+
+//alert()// we are using nodejs , not browser
+console.log(3+
+    3)
+console.log("Gaurav")//documentation MDN ans tc39.es
+
+let name="gaurav"
+let age=22
+let isloggedIn=false
+
+//number => 2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//unefine=>
+//symbol=>unique
+
+
+//object
+
+console.log(typeof "gaurav");
+console.log(typeof "undefined");//undefined
+console.log(typeof "null");//object
