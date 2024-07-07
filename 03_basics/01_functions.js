f22227864d2743237316920708e16c5b73b66747
Gaurav89998 <150925403+Gaurav89998@users.noreply.github.com>
2024-07-07 03:45:07 +0000

    function1

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -35,4 +35,34 @@ function loginUserMessage(username="sam"){
 }
 
 //console.log(loginUserMessage("hitseh"))
-console.log(loginUserMessage())
+//console.log(loginUserMessage())
+
+function calculateCartPrice(...num1){
+     return num1
+}
+
+//console.log(calculateCartPrice(200,400,500))
+
+const user={
+   username:"hitesh",
+   price:199
+}
+
+function handleObject(anyObject){
+   console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`)
+}
+
+//handleObject(user)
+handleObject({
+   username:"sam",
+   price:399
+})
+
+const myNewArray=[200,400,100,600]
+
+function returnSecondValue(getArray){
+   return getArray[1]
+}
+
+//console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200,400,500]))
